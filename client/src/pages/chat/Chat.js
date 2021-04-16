cc937294af47ceca064e364fb8dd3318ee884542
souravtecken <souravtecken@gmail.com>
2021-04-16 14:47:46 +0530

    Incoming messages bug fixed

--- client/src/pages/chat/Chat.js
+++ client/src/pages/chat/Chat.js
@@ -24,35 +24,90 @@ import { initializeChat } from '../../actions/chat';
 import { showAlert } from '../../utils/alert/Alert';
 import './chat.css';
 
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            socket: null,
+            chatData: null,
+            chatUsers: [],
+            mappedUsers: {},
+            currentChat: null,
+            currentMessage: ''
+        }
+        this.setSocket = (socket) => {
+            this.setState({ socket });
+        }
+        this.setChatData = (chatData) => {
+            this.setState({ chatData });
+        }
+        this.setChatUsers = (chatUsers) => {
+            this.setState({ chatUsers });
+        }
+        this.setMappedUsers = (mappedUsers) => {
+            this.setState({ mappedUsers });
+        }
+        this.setCurrentChat = (currentChat) => {
+            this.setState({ currentChat });
+        }
+        this.setCurrentMessage = (currentMessage) => {
+            this.setState({ currentMessage });
+        }
+        this.sendMessage = this.sendMessage.bind(this);
+        this.selectChat = this.selectChat.bind(this);
+        this.handleSendMessage = this.handleSendMessage.bind(this);
+    }
 
-const Chat = (props) => {
+    sendMessage(message, recipient) {
+        const messageObj = {
+            message,
+            recipient,
+            token: this.props.token
+        }
+        this.state.socket.emit('newMessage', messageObj, (ack) => {
+            if(!ack.success) return showAlert(ack.message, 'error');
+            this.setCurrentMessage('');
+            const updatedChatData = {...this.state.chatData};
+            updatedChatData[recipient].messages.push(ack.message);
+            this.setChatData(updatedChatData);
+        })
+    }
 
-    const [socket, setSocket] = useState(null);
-    const [chatData, setChatData] = useState(null);
-    const [chatUsers, setChatUsers] = useState([]);
-    const [mappedUsers, setMappedUsers] = useState({}); // key: userId, value: user details
-    const [currentChat, setCurrentChat] = useState(null); // the id of user whose chat is currently active
-    const [currentMessage, setCurrentMessage] = useState(''); // The text in the message input box
+    selectChat(id) {
+        if(!(id in this.state.chatData)) {
+            const updatedChatData = this.state.chatData;
+            updatedChatData[id] = {messages: []};
+            this.setChatData(updatedChatData);
+        }
+        this.setCurrentChat(id);
+    }
+
+    handleSendMessage() {
+        if(!this.state.currentMessage) return;
+        this.sendMessage(this.state.currentMessage, this.state.currentChat);
+    }
 
 
-    useEffect(() => {
+
+    componentDidMount() {
+        const self = this;
         const socket = io('http://localhost:8000');
         socket.on('connect', () => {
-            socket.emit('initialize', {id: props.user.id}, (ack) => {
+            socket.emit('initialize', {id: this.props.user.id}, (ack) => {
                 if(!ack.success) showAlert(ack.message, 'error');
             })
-            setSocket(socket);
+            self.setSocket(socket);
         });
         const config = {
             headers: {
                 'Content-Type': 'application/json',
-                'x-auth-token': props.token,
+                'x-auth-token': this.props.token,
             },
         };
         // Fetch user chat history
         client.post('/user/getChats', {}, config)
         .then(res => {
-            setChatData(res.data.chat);
+            this.setChatData(res.data.chat);
         })
         .catch(err => {
             console.log(err);
@@ -60,161 +115,134 @@ const Chat = (props) => {
         // Fetch doctors 
         client.post('/user/getChatUsers', {}, config)
         .then((res) => {
-            setChatUsers(res.data);
+            this.setChatUsers(res.data);
             const userObj = {};
             for(const user of res.data) userObj[user._id] = user;
-            setMappedUsers(userObj);
+            this.setMappedUsers(userObj);
         })
         .catch(err => {
             console.log(err);
         })
-    }, []);
-    
-    if(socket === null || chatData === null) return <Loading/>;
 
-    socket.on('newMessage', (chat) => {
-        const updatedMessages = {...chatData};
-        if(!(chat.from in updatedMessages))
-            updatedMessages[chat.from] = {messages: []};
-        updatedMessages[chat.from].messages.push(chat);
-        setChatData(updatedMessages);
-    })
-
-    const sendMessage = (message, recipient) => {
-        const messageObj = {
-            message,
-            recipient,
-            token: props.token
-        }
-        socket.emit('newMessage', messageObj, (ack) => {
-            if(!ack.success) return showAlert(ack.message, 'error');
-            setCurrentMessage('');
-            const updatedChatData = {...chatData};
-            updatedChatData[recipient].messages.push(ack.message);
-            setChatData(updatedChatData);
+        socket.on('newMessage', (chat) => {
+            const updatedMessages = {...this.state.chatData};
+            if(!(chat.from in updatedMessages))
+                updatedMessages[chat.from] = {messages: []};
+            updatedMessages[chat.from].messages.push(chat);
+            this.setChatData(updatedMessages);
         })
+        
     }
 
-    const selectChat = (id) => {
-        if(!(id in chatData)) {
-            const updatedChatData = chatData;
-            updatedChatData[id] = {messages: []};
-            setChatData(updatedChatData);
-        }
-        setCurrentChat(id);
-    }
+    render() {
+        if(this.state.socket === null || this.state.chatData === null) return <Loading/>;
 
-    const handleSendMessage = () => {
-        if(!currentMessage) return;
-        sendMessage(currentMessage, currentChat);
-    }
+        const chatHeader = (
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" noWrap>
+                        Chat
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        )
 
-
-    const chatHeader = (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" noWrap>
-                    Chat
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
-
-    const search = (
-        <Autocomplete
-            options={chatUsers}
-            getOptionLabel={option => option.name}
-            onChange={(event, user) => {
-                if(user) selectChat(user._id);
-            }}
-            renderOption={option => (                                            
-                <Fragment>
-                    <Avatar alt="doctor_display" src={option.profilePicture} /> &nbsp;&nbsp;
-                    {option.name}
-                </Fragment>
-            )}
-            renderInput={(params) => (
-                <Paper ref={params.InputProps.ref} className='chat-search-container'>
-                    <InputBase
-                        className='chat-search-input'
-                        placeholder="Search doctors"
-                        {...params.inputProps}
-                    />
-                    <IconButton>
-                        <SearchIcon />
-                    </IconButton>
-                </Paper>
-            )}
-        />
-    )
-
-    const chatList = (
-        <List>
-            {Object.keys(chatData).map(id => (
-                <Fragment key={id}>
-                    <ListItem 
-                        alignItems="flex-start" 
-                        className={`chat-list-item ${currentChat === id? 'active': ''}`}
-                        onClick={() => selectChat(id)}
-                    >
-                        <ListItemAvatar>
-                            <Avatar alt="doctor_display" src={mappedUsers[id].profilePicture} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={mappedUsers[id].name}
+        const search = (
+            <Autocomplete
+                options={this.state.chatUsers}
+                getOptionLabel={option => option.name}
+                onChange={(event, user) => {
+                    if(user) this.selectChat(user._id);
+                }}
+                renderOption={option => (                                            
+                    <Fragment>
+                        <Avatar alt="doctor_display" src={option.profilePicture} /> &nbsp;&nbsp;
+                        {option.name}
+                    </Fragment>
+                )}
+                renderInput={(params) => (
+                    <Paper ref={params.InputProps.ref} className='chat-search-container'>
+                        <InputBase
+                            className='chat-search-input'
+                            placeholder="Search doctors"
+                            {...params.inputProps}
                         />
-                    </ListItem>
-                    <Divider/>
-                </Fragment>
-            ))}            
-        </List>
-    )
-    
-    const chatMessages = (
-        <Fragment>
-            {currentChat ? (
-                (currentChat in chatData) ? (
-                    chatData[currentChat].messages.map((message) => {
-                        const timestamp = new Date(message.timestamp);
-                        return <div className={`chat-message ${message.from == props.user.id ? 'self': 'other'}`}>
-                            {message.message}
-                            <div className='chat-message-timestamp'>{timestamp.toLocaleString()}</div>
-                        </div>
-                    })
-                ) : null
-            ): null}
-        </Fragment>
-    )
-    
+                        <IconButton>
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
+                )}
+            />
+        )
 
-    return (
-        <Layout>
-            <Container>
-                <Grid container className='chat-container'>
-                    <Grid className='chat-container-grid left-container' item xs={4}>
-                        {chatHeader}
-                        {search}
-                        {chatList}
-                    </Grid>
-                    <Grid className='chat-container-grid right-container' item xs={8}>
-                        {chatMessages}
-                        <Paper className='chat-send-message-container'>
-                            <InputBase
-                                className='chat-send-message-input'
-                                placeholder="Send message"
-                                value={currentMessage}
-                                onChange={e => setCurrentMessage(e.target.value)}
+        const chatList = (
+            <List>
+                {Object.keys(this.state.chatData).map(id => (
+                    <Fragment key={id}>
+                        <ListItem 
+                            alignItems="flex-start" 
+                            className={`chat-list-item ${this.state.currentChat === id? 'active': ''}`}
+                            onClick={() => this.selectChat(id)}
+                        >
+                            <ListItemAvatar>
+                                <Avatar alt="doctor_display" src={this.state.mappedUsers[id].profilePicture} />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={this.state.mappedUsers[id].name}
                             />
-                            <IconButton onClick={handleSendMessage}>
-                                <SendIcon />
-                            </IconButton>
-                        </Paper>
-                    </Grid>
-                </Grid>                
-            </Container>
-        </Layout>
-    )
+                        </ListItem>
+                        <Divider/>
+                    </Fragment>
+                ))}            
+            </List>
+        )
+
+        const chatMessages = (
+            <Fragment>
+                {this.state.currentChat ? (
+                    (this.state.currentChat in this.state.chatData) ? (
+                        this.state.chatData[this.state.currentChat].messages.map((message) => {
+                            const timestamp = new Date(message.timestamp);
+                            return <div className={`chat-message ${message.from == this.props.user.id ? 'self': 'other'}`}>
+                                {message.message}
+                                <div className='chat-message-timestamp'>{timestamp.toLocaleString()}</div>
+                            </div>
+                        })
+                    ) : null
+                ): null}
+            </Fragment>
+        )
+
+        return (
+            <Layout>
+                <Container>
+                    <Grid container className='chat-container'>
+                        <Grid className='chat-container-grid left-container' item xs={4}>
+                            {chatHeader}
+                            {search}
+                            {chatList}
+                        </Grid>
+                        <Grid className='chat-container-grid right-container' item xs={8}>
+                            {chatMessages}
+                            <Paper className='chat-send-message-container'>
+                                <InputBase
+                                    className='chat-send-message-input'
+                                    placeholder="Send message"
+                                    value={this.state.currentMessage}
+                                    onChange={e => this.setCurrentMessage(e.target.value)}
+                                />
+                                <IconButton onClick={this.handleSendMessage}>
+                                    <SendIcon />
+                                </IconButton>   
+                            </Paper>
+                        </Grid>
+                    </Grid>                
+                </Container>
+            </Layout>
+        )
+    }
 }
+    
 
 const mapStateToProps = (state) => {
     return {
