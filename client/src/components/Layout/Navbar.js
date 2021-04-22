4234cfe4300585be8ec24778f317709f177f3e10
souravtecken <souravtecken@gmail.com>
2021-04-22 13:44:08 +0530

    Added logout button

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -3,18 +3,33 @@ import {
     AppBar, Toolbar, Typography,
     Button
 } from '@material-ui/core';
+import {connect} from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    console.log(props.auth);
+    const handleLogOut = () => {
+        window.localStorage.removeItem('practoUser');
+        window.location.reload();
+    }
     return (        
-        <AppBar position="static">
+        <AppBar position="static" style={{display: 'flex', flexGrow: '1'}}>
             <Toolbar>                                
-                <Typography variant="h6">
+                <Typography variant="h6" style={{display: 'flex', flexGrow: '1'}}>
                     Practose
                 </Typography>
-                {/* <Button color="inherit">Login</Button> */}
+                {props.auth.loggedIn ?
+                    <Button color="inherit" style={{float: 'right'}} onClick={handleLogOut}>Logout</Button>:
+                    null
+                }                
             </Toolbar>
         </AppBar>
     )
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);
